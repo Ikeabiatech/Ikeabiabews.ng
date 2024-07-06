fa0539c561f2ddc050f842b52f65aa16a7bcb572
Ikeabiatech <frankchibuzo22@gmail.com>
2024-07-06 16:31:01 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+
+document.addEventListener("DOMContentLoaded", function() {
+    const navToggle = document.getElementById('nav-toggle');
+    const navLinks = document.getElementById('nav-links');
+
+    navToggle.addEventListener('click', function() {
+        navLinks.classList.toggle('showing');
+    });
+});
